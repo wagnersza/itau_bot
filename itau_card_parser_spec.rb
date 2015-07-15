7f2862b5dedfd7856e6d948e71d9c6848e6cec49
Wagner Souza <wagnersza@gmail.com>
2015-07-15 15:59:40 -0300

    funcionando poupanca cc

---
+++ itau_card_parser_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe ItauCardParser do
+  it 'returns all the transactions in the right format' do
+    transactions = subject.parse(itau_card_html)
+
+    expected_first_transaction = {
+      :date => "2015-01-02",
+      :memo => "RSHOP-ANTONIO LIS-02/01",
+      :value => -4310,
+    }
+
+    expected_last_transaction = {
+      date: "2015-01-19",
+      memo: "RSHOP-NAZARENO RO-19/01",
+      value: -7200
+    }
+
+    expect(transactions.count).to eq 63
+    expect(transactions.first).to eq expected_first_transaction
+    expect(transactions.last).to eq expected_last_transaction
+
+    transactions.each do |transaction|
+      expect(transaction[:date]).to be
+      expect(transaction[:memo]).to be
+      expect(transaction[:value] != 0).to be true
+    end
+  end
+end
---
+++ lib/itau_card_parser.rb
@@ -0,0 +1,72 @@
+require 'active_support/all'
+
+class ItauCardParser
+  def parse(page_html)
+    html = Nokogiri.parse(page_html)
+    trans = html.css("#buscaPesquisaOnline tr").select {|tr| tr.css(".EXTlinhaPar, .EXTlinhaImpar").count > 0}
+    table_cells = trans.map {|tr| tr.css("td").select do |td|
+      text = td.text.strip
+      text != "" && text != " D" && text != " C"
+    end.map {|td| td.text.strip } }
+
+    fix_year_leaps(format_transactions(table_cells).reject {|transaction| blacklisted? transaction[:memo]})
+  end
+
+  def fix_year_leaps(transactions)
+    last_year = (DateTime.now - 1.year).year.to_s
+    current_year = DateTime.now.year.to_s
+
+    current_month = DateTime.parse(transactions.last[:date]).month
+    transactions.reverse.each do |tx|
+      if (tx_month = DateTime.parse(tx[:date]).month) <= current_month
+        current_month = tx_month
+      else
+        tx[:date].gsub! current_year, last_year
+      end
+    end.reverse
+  end
+
+  def format_transactions(transactions)
+    transactions.map do |transaction|
+      {
+        date: parse_date(transaction[0]),
+        memo: parse_memo(transaction[1]),
+        value: parse_value(transaction)
+      }
+    end
+  end
+
+  def parse_memo(text)
+    ActiveSupport::Inflector.transliterate(text, "").strip
+  end
+
+  def parse_value(transaction)
+    if transaction.last == "-"
+      # ["02/01", "RSHOP-ANTONIO LIS-02/01", "43,10", "-"]
+      - transaction[-2].gsub(/[\.,]/, "").to_i
+    else
+      # ["02/01", "TBI .-1/500", "200,00"]
+      transaction.last.gsub(/[\.,]/, "").to_i
+    end
+  end
+
+  def parse_date(text)
+    day, month = text.split("/")
+
+    "#{DateTime.now.year}-#{month}-#{day}"
+  end
+
+  def blacklisted?(memo)
+    [
+      "(-) SALDO A LIBERAR",
+      "SALDO FINAL DISPONIVEL",
+      "SDO",
+      "SDO CTA/APL AUTOMATICAS",
+      "S A L D O",
+      "SALDO DO DIA",
+      "REND PAGO APLIC AUT APR",
+      "SALDO ANTERIOR",
+      "TRANSFERENCIA SALDO"
+    ].include? memo
+  end
+end
